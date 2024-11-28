a9bb19a9963d7fe76d138b29563c08c86109e269
HanaGt <hana.guta-ug@aau.edu.et>
2024-11-28 22:56:44 +0300

    fix: update the button style

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,24 +1,25 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+
 import {
   NavigationMenu,
   NavigationMenuList,
   NavigationMenuItem,
   NavigationMenuLink,
 } from "@/components/ui/navigation-menu";
+import Button from "../ui/button";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 ">
-      <div className="max-w-7x px-4 sm:px- lg:px-8 flex  justify-between py-4">
-        <div className="flex ">
+    <header className="sticky top-0 z-50">
+      <div className="max-w-7x px-4 sm:px- lg:px-8 flex justify-between py-4">
+        <div className="flex">
           <Image src="/logo.png" alt="Logo" width={244} height={67} />
         </div>
 
         <NavigationMenu>
           <NavigationMenuList className="flex items-center space-x-10 text-gray-700 font-bold">
             <NavigationMenuItem>
-              <NavigationMenuLink href="/" className="hover:text-secondary ">
+              <NavigationMenuLink href="/" className="hover:text-secondary">
                 Home
               </NavigationMenuLink>
             </NavigationMenuItem>
@@ -54,13 +55,15 @@ const Header = () => {
                 Testimonials
               </NavigationMenuLink>
             </NavigationMenuItem>
-            <Button variant="default" asChild>
-              <a
-                href="/contact"
-                className="hover:bg-primary border border-primary font-bold text-white px-4 py-2 rounded-lg transition-all !important"
-              >
-                Get in Touch
-              </a>
+
+            <Button
+              as="a"
+              href="/contact"
+              variant="outline"
+              rounded="rounded-full"
+              className="px-4 py-2"
+            >
+              Get in Touch
             </Button>
           </NavigationMenuList>
         </NavigationMenu>
