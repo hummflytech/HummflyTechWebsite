e2a61dfbd289d4274841d3c6917e795f4f7ab550
MisganaGetachew <misganagetachew111@gmail.com>
2024-12-07 15:03:24 +0300

    fix routing issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Footer from "@/components/Home/footer";
 import Header from "@/components/common/header";
 
 const geistSans = localFont({
@@ -28,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header />
+
         {children}
       </body>
     </html>
