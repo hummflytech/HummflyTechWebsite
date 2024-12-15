fbb3bd19dbc8603426c8a980b908f06b15e20fea
Ephraimdebel <ephraimdebel@gmail.com>
2024-12-15 20:46:45 +0300

    bug fix

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,5 +1,5 @@
 // pages/form.js
-import React from 'react';
+import React from "react";
 
 const FormPage = () => {
   return (
@@ -8,7 +8,7 @@ const FormPage = () => {
         src="https://docs.google.com/forms/d/e/1FAIpQLSd4PNI0eV5qZK71GDpqSyQPOKa6oIsQfj7VnHH-XP7JvMqBQw/viewform?embedded=true"
         width="100%"
         height="1000"
-        style={{ border: 'none' }}
+        style={{ border: "none" }}
         title="Google Form"
       >
         Loading…
